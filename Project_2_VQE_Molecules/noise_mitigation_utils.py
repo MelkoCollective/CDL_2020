79d59dcd37e207383dfa7de5cc210395c8f552ae
Andrew Patterson <andrew.pattersonap24@googlemail.com>
2020-07-17 16:39:17 +0100

    liner fit bug fix

--- Project_2_VQE_Molecules/noise_mitigation_utils.py
+++ Project_2_VQE_Molecules/noise_mitigation_utils.py
@@ -23,16 +23,18 @@ class NoiseMitigation:
 
     @staticmethod
     def make_noisy(circ: tq.QCircuit):
-        for i, moment in enumerate(circ.moments):
-            circ = circ.insert_gates([i], NoiseMitigation.noise_on_qubits(circ.qubits))
+        noisy_ops = NoiseMitigation.noise_on_qubits(circ.qubits)
+        n_noisy_ops = len(noisy_ops)
+        for i, moment in reversed(list(enumerate(circ.moments))):
+            circ = circ.insert_gates([i + j for j in range(n_noisy_ops)], noisy_ops)
         return circ
 
-    def zero_noise_expansion(self, H: tq.QubitHamiltonian, U: tq.QCircuit, vars: dict, samples: int = None):
+    def zero_noise_expansion(self, H: tq.QubitHamiltonian, U: tq.QCircuit, variables: dict, samples: int = None):
         E = tq.ExpectationValue(H=H, U=U)
-        low_noise = tq.simulate(E, variables=vars, backend=self.backend, device=self.device, samples=samples)
+        low_noise = tq.simulate(E, variables=variables, backend=self.backend, device=self.device, samples=samples)
         noisy_U = NoiseMitigation.make_noisy(U)
         noisy_E = tq.ExpectationValue(H=H, U=noisy_U)
-        noisy = tq.simulate(noisy_E, variables=vars, backend=self.backend, device=self.device, samples=samples)
+        noisy = tq.simulate(noisy_E, variables=variables, backend=self.backend, device=self.device, samples=samples)
         x1 = U.canonical_depth
         x2 = noisy_U.canonical_depth
         zero_n = NoiseMitigation.linear_fit([x1, x2], [low_noise, noisy])
@@ -44,8 +46,20 @@ class NoiseMitigation:
         ys = np.array(ys)
         model = LinearRegression()
         model.fit(xs, ys)
-        zero_n = model.predict(0)
-        return zero_n
+        zero_n = model.predict(np.array([0]).reshape(1, -1))
+        return zero_n[0]
+
+
+if __name__ == '__main__':
+    from utility import *
+    x1 = 30
+    x2 = 60
+    y1 = 0.5
+    y2 = 1
+    print(NoiseMitigation.linear_fit([x1, x2], [y1, y2]))
+
+
+
 
 
 
