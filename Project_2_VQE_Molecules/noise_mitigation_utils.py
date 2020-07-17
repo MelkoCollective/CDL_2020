18f9f194e465d0374219f87fbf037482334fbe64
Andrew Patterson <andrew.pattersonap24@googlemail.com>
2020-07-17 01:23:20 +0100

    clean

--- Project_2_VQE_Molecules/noise_mitigation_utils.py
+++ Project_2_VQE_Molecules/noise_mitigation_utils.py
@@ -36,7 +36,7 @@ class NoiseMitigation:
         x1 = U.canonical_depth
         x2 = noisy_U.canonical_depth
         zero_n = NoiseMitigation.linear_fit([x1, x2], [low_noise, noisy])
-        return zero_n
+        return zero_
 
     @staticmethod
     def linear_fit(xs: Union[np.ndarray, list], ys: Union[np.ndarray, list]):
