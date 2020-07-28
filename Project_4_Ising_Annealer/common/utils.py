1b9adc93fee5ddbcb6279b230ab38bf8e1210013
Hayk Sargsyan <52532457+hay-k@users.noreply.github.com>
2020-07-28 23:52:02 +0400

    Task 2: initial implementation

---
+++ Project_4_Ising_Annealer/common/utils.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def exp_decay_schedule(N, T_i, T_f):
+    t = np.arange(N+1)
+    return T_i * ((T_f/T_i) ** (t/N))
+
+
+def anneal(ising, schedule, mc_steps=1):
+    for T in schedule:
+        for _ in range(mc_steps):
+            E = ising.mc_step(T)
+
+    return ising.energy(), ising.get_spins()
