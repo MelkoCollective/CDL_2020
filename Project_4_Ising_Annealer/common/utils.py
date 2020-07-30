bb112f0dd7a3aaa3cf08ba5362f633e86b429ff7
Hayk Sargsyan <52532457+hay-k@users.noreply.github.com>
2020-07-30 19:17:10 +0400

    Add functionality to do oscillations on a decay schedule

--- Project_4_Ising_Annealer/common/utils.py
+++ Project_4_Ising_Annealer/common/utils.py
@@ -33,20 +33,38 @@ def inv_decay_schedule(N, Ti, Tf):
     return Ti / (1 + (Ti / Tf - 1) * time / N), time
 
 
-def lin_decay_schedule(N, Ti, Tf):
+def poly_decay_schedule(N, Ti, Tf, degree):
     """
-    Construct a temperature sequence for linear decay schedule
+    Construct a temperature sequence for polynomial decay schedule given by a*x^degree + b
     
     Arguments:
         N: number of discrete time points
         Ti: initial temperature
         Tf: final temperature
+        degree: positive integer. The degree of the polynomial
         
     Returns:
         The sequence of temperature updates and the list of discrete time points
     """
     time = np.arange(N + 1)
-    return Ti + (Tf - Ti) * time / N, time
+    return Ti + (Tf - Ti) * (time / N) **degree, time
+
+
+def osc_decay_schedule(sched, time, a, b):
+    """
+    Add oscilations to an annealing schedule
+    
+    Arguments:
+        sched: an annealing schedule
+        time: the discrete time points of the schedule
+        a: control the aplitude of oscillations
+        b: control the number of oscillations 
+        
+    Returns:
+        The sequence of temperature updates and the list of discrete time points
+    """
+    N = time.shape[0] - 1
+    return sched * (a * np.sin(time / (N / b)) **2 + 1), time
 
 
 def anneal(ising, schedule, mc_steps=1):
